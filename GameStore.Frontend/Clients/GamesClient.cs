6013ec5a6fe881ad7dc97425e17dc5bd056aae84
anek05 <andreas.eklund94@gmail.com>
2024-05-06 02:31:43 +0200

    added editgame form

---
+++ GameStore.Frontend/Clients/GamesClient.cs
@@ -0,0 +1,33 @@
+﻿using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients;
+
+public class GamesClient
+{
+    private readonly List<GameSummary> games = 
+    [
+        new(){
+            Id = 1,
+            Name = "Elden Ring",
+            Genre = "Adventure",
+            Price = 59.99M,
+            ReleaseDate = new DateOnly(2022, 01, 19)
+        },
+        new(){
+            Id = 2,
+            Name = "Guild Wars 2",
+            Genre = "MMORPG",
+            Price = 49.99M,
+            ReleaseDate = new DateOnly(2012, 08, 22)
+        },
+        new(){
+            Id = 3,
+            Name = "ROcket League",
+            Genre = "Sports",
+            Price = 19.99M,
+            ReleaseDate = new DateOnly(2013, 10, 05)
+        }
+    ];
+
+    public GameSummary[] GetGames() => [.. games];
+}
---
+++ GameStore.Frontend/Clients/GenresClient.cs
@@ -0,0 +1,32 @@
+﻿using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients;
+
+public class GenresClient
+{
+    private readonly Genre[] genres = 
+    [
+        new(){
+            Id = 1,
+            Name = "Fighting"
+        },
+        new(){
+            Id = 2,
+            Name = "Adventure"
+        },
+        new(){
+            Id = 3,
+            Name = "MMORPG"
+        },
+        new(){
+            Id = 4,
+            Name = "Sports"
+        },
+        new(){
+            Id = 5,
+            Name = "FPS"
+        }
+    ];
+
+    public Genre[] GetGenres() => genres;
+}
